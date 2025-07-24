3486eaab9efd8bc4a6d40f1b4de8b203465b7ce3
Mikalai Biadrytski <nickbiadrytski@gmail.com>
2025-07-24 17:35:58 +0300

    Allure metadata for API test

--- api/tests/detect-face.spec.ts
+++ api/tests/detect-face.spec.ts
@@ -1,12 +1,19 @@
 import { APIResponse } from '@playwright/test';
+import { epic, story, suite, tags } from 'allure-js-commons';
 
 import { commonFixtures as testFaceApi } from '../../common/fixtures/common-fixtures';
 import { detectFaceBasePayload } from '../data/face-api/payloads/detect-face-base-paylaod';
+
 
 testFaceApi.describe('Regula Face SDK Web API', () => {
 	testFaceApi(
 		'Face cannot be detected with an image without a face',
 		async ({ faceApiClientSteps }) => {
+			await suite('API');
+			await epic('Regula Face SDK Web API');
+			await story('Detect face');
+			await tags('API', 'FaceAPI');
+
 			const apiResponse: APIResponse = await faceApiClientSteps.detectFace(
 				detectFaceBasePayload(),
 				200,
